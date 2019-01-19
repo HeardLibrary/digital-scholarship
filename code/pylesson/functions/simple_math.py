bca5965244c966200adb10dac30c5cb3064d5eb5
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2019-01-19 09:37:09 -0600

    added simple module and package examples

---
+++ code/pylesson/functions/simple_math.py
@@ -0,0 +1,15 @@
+def addition(firstNumber, secondNumber):
+    answer = firstNumber + secondNumber
+    return answer
+
+def subtraction(firstNumber, secondNumber):
+    answer = firstNumber - secondNumber
+    return answer
+
+def multiplication(firstNumber, secondNumber):
+    answer = firstNumber * secondNumber
+    return answer
+
+def division(firstNumber, secondNumber):
+    answer = firstNumber / secondNumber
+    return answer
---
+++ code/pylesson/functions/simple_string.py
@@ -0,0 +1,6 @@
+def concatenation(firstString, secondString):
+    answer = firstString + secondString
+    return answer
+
+def findLength(string):
+    return len(string)
