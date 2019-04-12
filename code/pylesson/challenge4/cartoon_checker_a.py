1351f9bfd78aedbc05e93c79f8a993f376e13af4
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2019-04-12 09:17:39 -0500

    Update cartoon_checker_a.py

--- code/pylesson/challenge4/cartoon_checker_a.py
+++ code/pylesson/challenge4/cartoon_checker_a.py
@@ -1,24 +1,13 @@
 import csv
 import requests
 
-# To use a local file, comment out this part, and uncomment the next part
-r = requests.get('https://github.com/HeardLibrary/digital-scholarship/raw/master/code/pylesson/challenge4/cartoons.csv')
-fileText = r.text.split('\n')
-if fileText[len(fileText)-1] == '':
-    fileText = fileText[0:len(fileText)-1]
-readerObject = csv.DictReader(fileText)
-cartoon = []
-for row in readerObject:
-    cartoon.append(row)
 
-'''
 fileObject = open('cartoons.csv', 'r', newline='', encoding='utf-8')
 readerObject = csv.DictReader(fileObject)
 cartoon = []
 for row in readerObject:
     cartoon.append(row)
 fileObject.close()
-'''
 
 inputCharacterName = input("What's the name of the character? ")
 
