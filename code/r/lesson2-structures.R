d7e082c745ef01c3a127855b5ecdb744b457a1d2
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2020-01-24 12:15:08 -0600

    add commands for examining data frames

--- code/r/lesson2-structures.R
+++ code/r/lesson2-structures.R
@@ -68,12 +68,23 @@ organism_info$animal # column by name
 organism_info[2,1] # cell by row, column
 organism_info$animal[4] # cell by column name and position in column
 
-# loading data from a CSV into a data frame
+# Loading data from a CSV into a data frame
 my_data_frame <- read.csv("~/test.csv")  # by a path (Mac home directory)
 my_data_frame <- read.csv("c:\temp\test.csv")  # by a path (Windows C: drive)
 my_data_frame <- read.csv(file.choose())  # by a file-choosing dialog
 # from a URL:
 my_data_frame <- read.csv("https://gist.githubusercontent.com/baskaufs/1a7a995c1b25d6e88b45/raw/4bb17ccc5c1e62c27627833a4f25380f27d30b35/t-test.csv") 
+
+# Examining a data frame
+
+# head() shows the first 6 rows
+# tail() shows the last 6 rows
+# names() returns the column names
+# str() describes the structure of the data frame with information about each column
+head(my_data_frame)
+tail(my_data_frame)
+names(my_data_frame)
+str(my_data_frame)
 
 # -------------------
 # Homework
