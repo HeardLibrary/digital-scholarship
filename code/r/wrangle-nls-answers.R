786032b273d533ab5d70f3e4077d204e7f8e84ba
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2020-02-18 09:48:01 -0600

    add missing value calcs for all variables

--- code/r/wrangle-nls-answers.R
+++ code/r/wrangle-nls-answers.R
@@ -1,5 +1,5 @@
 # ------------------
-# Homework assignment #1: Select subset of responses and merge datasets
+# Assignment #1: Select subset of responses and merge datasets
 # ------------------
 
 # We want to pull out the following responses from the two listed datasets,
@@ -8,21 +8,21 @@
 
 # DS0001
 # AID Unique participant identifier
-# H1PF1 S18Q1 Mom warm and loving p. 364
-# H1PF2 S18Q2 Mom encurages independence p. 364
-# H1PF3 S18Q3 Mom discusses ethics p. 365
-# H1PF4 S18Q4 Mom good communication p. 365
-# H1PF5 S18Q5 Mom good relationship p. 366
-# H1PF23 S18Q23 Dad warm and loving p. 375
-# H1PF24 S18Q24 Dad good communication p. 376
-# H1PF25 S18Q25 Dad good relationship p. 376
+# H1PF1 S18Q1 Mom warm and loving p. 364 missing values 6-8
+# H1PF2 S18Q2 Mom encurages independence p. 364 missing values 6-8
+# H1PF3 S18Q3 Mom discusses ethics p. 365 missing values 6-9
+# H1PF4 S18Q4 Mom good communication p. 365 missing values 6-9
+# H1PF5 S18Q5 Mom good relationship p. 366 missing values 6-9
+# H1PF23 S18Q23 Dad warm and loving p. 375 missing values 6-9
+# H1PF24 S18Q24 Dad good communication p. 376 missing values 6-9
+# H1PF25 S18Q25 Dad good relationship p. 376 missing values 6-9
 # 
 # DS0022
 # AID Unique participant identifier
-# H4TO1 S23Q1 Ever smoked entire cigarette p. 399
-# H4TO2 S23Q2 Age 1st smoked entire cigarette p. 399
-# H4TO3 S23Q3 Ever smoked cigs regularly p. 400
-# H4TO4 S23Q4 Age smoled cigs regularly p. 401
+# H4TO1 S23Q1 Ever smoked entire cigarette p. 399 missing values 6-8
+# H4TO2 S23Q2 Age 1st smoked entire cigarette p. 399 missing values 96-98
+# H4TO3 S23Q3 Ever smoked cigs regularly p. 400 missing values 6-8
+# H4TO4 S23Q4 Age smoled cigs regularly p. 401 missing values 96-98
 # H4TO5 S23Q5 Num days smokd cigs lst 30 days p. 402
 # H4GH6 S4Q6 Weight lbs p. 50 missing values 996 and 998
 # H4GH5F S4Q5F Height feet p. 49 missing value 98
@@ -60,7 +60,7 @@ write_csv(combined_outer_join, "outer_raw.csv", na = "NA", append = FALSE, col_n
 write_csv(combined_inner_join, "inner_raw.csv", na = "NA", append = FALSE, col_names = TRUE, quote_escape = "double")
 
 # ------------------
-# Homework assignment #2: Calculate a BMI column
+# Assignment #2: Calculate a BMI column
 # ------------------
 
 # Easiest to do by creating a column for height in meters and column for mass in kg first.
@@ -83,6 +83,11 @@ summary(no_na$H4GH6)
 # calculate BMI
 bmi_tibble <- inner_raw %>%
   # replace missing data numbers with NA
+  mutate(H4TO1 = replace(H4TO1, H4TO1>=6, NA)) %>% # age missing value 6-8
+  mutate(H4TO2 = replace(H4TO2, H4TO2>=96, NA)) %>% # age missing value 96-98
+  mutate(H4TO3 = replace(H4TO3, H4TO3>=6, NA)) %>% # age missing value 6-8
+  mutate(H4TO4 = replace(H4TO4, H4TO4>=96, NA)) %>% # age missing values 96-98
+  mutate(H4TO5 = replace(H4TO5, H4TO5>=32, NA)) %>% # days/month missing values greater than 31
   mutate(H4GH6 = replace(H4GH6, H4GH6>=990, NA)) %>% # weight missing values 996 and 998
   mutate(H4GH5F = replace(H4GH5F, H4GH5F==98, NA)) %>% # feet missing value 98
   mutate(H4GH5I = replace(H4GH5I, H4GH5I==98, NA)) %>% # inches missing value 98
@@ -93,7 +98,7 @@ bmi_tibble <- inner_raw %>%
   mutate(bmi = mass_kg/height_m^2) # calculate BMI
 
 # ------------------
-# Homework assignment #3: Calculate a maternal closeness column
+# Assignment #3: Calculate a maternal closeness column
 # ------------------
 
 # change missing values to NA, then calculate maternal closeness
@@ -103,6 +108,9 @@ with_maternal <- bmi_tibble %>%
   mutate(H1PF3 = replace(H1PF3, H1PF3>=6, NA)) %>%
   mutate(H1PF4 = replace(H1PF4, H1PF4>=6, NA)) %>%
   mutate(H1PF5 = replace(H1PF5, H1PF5>=6, NA)) %>%
+  mutate(H1PF23 = replace(H1PF23, H1PF23>=6, NA)) %>%
+  mutate(H1PF24 = replace(H1PF24, H1PF24>=6, NA)) %>%
+  mutate(H1PF25 = replace(H1PF25, H1PF25>=6, NA)) %>%
   mutate(maternal_closeness = ifelse(!H1PF1==1 & !is.na(H1PF1), 0, ifelse(H1PF1==1, 1, NA)))
 
 write_csv(with_maternal, "mutated_data.csv", na = "NA", append = FALSE, col_names = TRUE, quote_escape = "double")
