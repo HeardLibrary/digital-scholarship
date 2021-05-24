33fecbb36f73817120fcd2d2c0f7157fb6db274f
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2021-05-23 20:40:34 -0500

    change default download filename for JSON file

--- script/wikidata/wikidata-csv2rdf-metadata.js
+++ script/wikidata/wikidata-csv2rdf-metadata.js
@@ -354,7 +354,7 @@ function createJSON() {
     $("#output-json-contents").text(string);
     // trigger file download - added by Mark Denning - 2021-04-19
     document.getElementById("json-download").setAttribute("href","data:application/text," + string)
-    document.getElementById("json-download").setAttribute("download",document.getElementById("csv-filename").value+".json")
+    document.getElementById("json-download").setAttribute("download","csv-metadata.json")
     document.getElementById("json-download").click()
     // un-hide copy to clipboard button if it is hidden
     $("#json-clip").removeAttr('hidden');
