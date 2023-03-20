44cc1007faecf8310049f9554c34703f6336bb04
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2023-03-20 13:04:34 -0500

    Create r_practice_assignment_1.R

---
+++ code/codegraf/011/r_practice_assignment_1.R
@@ -0,0 +1,43 @@
+# Practice assignment 1.
+
+# In the code that you write, for each object use a meaningful name in “snake case”.
+# Write your code in the space after the comment prompt for each problem. Write any 
+# verbal answers to the problems as comments following the prompt.
+
+# Problem 1. Create a vector object containing the names of the days of the week.
+
+
+# Problem 2. Create a sequence of the numbers for the days of the year (1=the first day, 
+# 365=the last day). Assign that sequence of numbers to a vector object.
+
+
+# Problem 3. Write the expression for Wednesday using your “day of the week” vector
+# using an appropriate index number for the Wednesday item in the vector.
+
+
+# Problem 4. Create a vector containing numbers from 0 to 100. Each number represents 
+# the radius of a circle. Calculate the areas of those circles and assign the answer 
+# to another vector. Note: There are very few “predefined” variables in R, but pi is 
+# one of them. You can use pi in your code without defining its value. However, you 
+# can also overwrite its value by assigning some other object the name pi. Additional 
+# note: / is the operator for division, * is the operator for multiplication, 
+# and ^ is the operator for exponentiation (i.e. r^2 is r squared).
+
+
+# Problem 5. The number of dogs in five households is: 2, 0, 1, 0, 1. The number of 
+# cats in the same five households is: 1, 0, 0, 2, 3. Assign the number of dogs to 
+# one vector and the number of cats to another vector. Calculate the number of 
+# pets (dogs plus cats) in the five households and assign the answer to another vector.
+
+
+# Problem 6. Go to the example link from the Posit Cloud quickstart lesson: 
+# https://github.com/HeardLibrary/digital-scholarship/blob/master/code/r/use_case_examples.R
+# Load the script into a new R script the text editor pane. Run the three installation
+# lines at the beginning of the script. This may take some time, so be patient. 
+# Then step through the remaining lines of the script one at a time and see what happens.
+# Explain what the difference is between 'install.packages("tidyverse")' and
+# 'library(tidyverse)'. Why do you only need to do the first command once, but you 
+# need to do the second command every time you restart the script?
+
+
+
