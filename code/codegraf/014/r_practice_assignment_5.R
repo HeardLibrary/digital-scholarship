11bbf7f1e159c053e53b92a6c81cb53193946ffb
Steve Baskauf <steve.baskauf@vanderbilt.edu>
2023-09-13 16:40:00 -0500

    Update r_practice_assignment_5.R

--- code/codegraf/014/r_practice_assignment_5.R
+++ code/codegraf/014/r_practice_assignment_5.R
@@ -10,26 +10,27 @@
 
 schools_tibble <- read_csv("https://raw.githubusercontent.com/HeardLibrary/digital-scholarship/master/data/gis/wg/Metro_Nashville_Schools.csv")
 
-# Problem 2. For each row where the School Level column has a value of "Elementary School", 
-# replace "Elementary School" with "Primary School". Remember that when a column header
+
+# Problem 2. Create a new column in the table called "Pacific Islanders" and populate it with a 
+# value of "present" if there is a number in the "Native Hawaiian or Other Pacific Islander" column 
+# and with a value of "absent" if there is a missing value. Remember that when a column header
 # contains a space, you must surround its name with backticks (`), which are different from 
 # single quotes (').
 
 
-# Problem 3. Create a new column in the table called "Pacific Islanders" and populate it with a 
-# value of "present" if there is a number in the "Native Hawaiian or Other Pacific Islander" column 
-# and with a value of "absent" if there is a missing value.
-
-
-# Problem 4. Sort the resulting tibble from the school with the most students to the 
+# Problem 3. Sort the resulting tibble from the school with the most students to the 
 # school with the least.
 
 
-# Problem 5. Use the select() function to create a new data frame that contains only the School Level, 
+# Problem 4. Use the select() function to create a new data frame that contains only the School Level, 
 # School Name, total number of students, and Pacific Islanders columns.
 
 
-# Problem 6. Combine all of the steps in the previous problems into a single pipeline using 
+# Problem 5. Combine steps 1-4 in the previous problems into a single pipeline using 
 # the %>% pipe operator. Assign the result to a tibble with a different name from the previuos one. 
 # Verify that this tibble is the same as the result that you got after problem 5.
+
+
+# Problem 6. For each row where the School Level column has a value of "Elementary School", 
+# replace "Elementary School" with "Primary School".
 
